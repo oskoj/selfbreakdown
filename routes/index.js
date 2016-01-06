0d692880ecf42c392f9aa91ffc7f227586ed06a8
Jonas Oskö <jonas.osko@beamonpeople.se>
2016-01-06 01:28:07 +0100

    add users to mongodb

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,30 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+router.get('/newuser', function(req, res) {
+  res.render('newuser', {
+    title: 'Add new user'
+  });
+});
+
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  var collection = db.get('usercollection');
+
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  }, function(err, doc) {
+    if (err) {
+      res.send("Oops...error! :(")
+    } else {
+      res.redirect("/userlist");
+    }
+  });
+});
+
 module.exports = router;
