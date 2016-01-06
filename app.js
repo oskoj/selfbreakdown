5752ee8440b71ca0a46480cd649f7872cf872da6
Jonas Oskö <jonas.osko@beamonpeople.se>
2016-01-06 01:08:01 +0100

    mongodb read endpoint

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/selfbreakdown');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -18,9 +22,16 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', {
+    title: 'hello world'
+  });
+});
+
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e, docs) {
+    res.render('userlist', {
+      "userlist": docs
+    });
+  });
 });
 
 module.exports = router;
